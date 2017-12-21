bb798370695b110a05f62032d762d9492c62115c
Anton Bozhinov <anton.bozhinov@softwaregroup.com>
2017-12-21 17:12:48 +0200

    add eslint to generate-service module

--- generate-service/index.js
+++ generate-service/index.js
@@ -6,38 +6,38 @@ const path = require('path');
 
 const CHOICES = fs.readdirSync(`${__dirname}/templates`);
 const QUESTIONS = [
-  {
-    name: 'project-choice',
-    type: 'list',
-    message: 'What service would you like to generate?',
-    choices: CHOICES,
-  },
+    {
+        name: 'project-choice',
+        type: 'list',
+        message: 'What service would you like to generate?',
+        choices: CHOICES,
+    },
 ];
 
 if (!process.argv[2]) {
-  QUESTIONS.push({
-    name: 'project-name',
-    type: 'input',
-    message: 'Project name:',
-    validate(input) {
-      if (/^([A-Za-z\-_\d])+$/.test(input)) return true;
-      return 'Project name may only include letters, numbers, underscores and hashes.';
-    },
-  });
+    QUESTIONS.push({
+        name: 'project-name',
+        type: 'input',
+        message: 'Project name:',
+        validate(input) {
+            if (/^([A-Za-z\-_\d])+$/.test(input)) return true;
+            return 'Project name may only include letters, numbers, underscores and hashes.';
+        },
+    });
 }
 
 const CURR_DIR = process.cwd();
 
 inquirer.prompt(QUESTIONS)
-  .then((answers) => {
-    const cliArg = process.argv[2];
-    const projectChoice = answers['project-choice'];
-    const projectName = answers['project-name'];
-    const templatePath = `${__dirname}/templates/${projectChoice}`;
+    .then((answers) => {
+        const cliArg = process.argv[2];
+        const projectChoice = answers['project-choice'];
+        const projectName = answers['project-name'];
+        const templatePath = `${__dirname}/templates/${projectChoice}`;
 
-    fs.copySync(templatePath, `${CURR_DIR}/${cliArg || projectName}`);
-    fs.ensureSymlinkSync(path.resolve(__dirname, '../modules'), `${CURR_DIR}/${projectName}/modules`);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+        fs.copySync(templatePath, `${CURR_DIR}/${cliArg || projectName}`);
+        fs.ensureSymlinkSync(path.resolve(__dirname, '../modules'), `${CURR_DIR}/${projectName}/modules`);
+    })
+    .catch((err) => {
+        console.error(err); // eslint-disable-line
+    });
