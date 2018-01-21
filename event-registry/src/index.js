226d48c5b86dcb093206dc164495922709489aa0
anton.bozhinov <anton.bozhinov@softwaregroup.com>
2018-01-21 20:07:21 +0200

    add redis event-registry

--- event-registry/src/index.js
+++ event-registry/src/index.js
@@ -1,19 +1,24 @@
 const app = require('express')();
 const broker = require('message-broker'); // eslint-disable-line
 const Logger = require('logger'); // eslint-disable-line
+const redis = require('redis');
 
 const logger = new Logger('event-registry');
 
 module.exports = () => {
     const PORT = process.env.PORT || 3000;
+    const { REDIS_HOST, REDIS_PORT } = process.env;
     const { REGISTER_EVENT } = broker.nativeEvents;
 
-    broker.subscribe(REGISTER_EVENT);
+    const client = redis.createClient(REDIS_PORT || 6379, REDIS_HOST);
 
-    broker.on(REGISTER_EVENT, (data) => {
-        console.log(data.content.toString());
+    client.on('error', (err) => {
+        logger.logE('REDIS_ERROR', err);
     });
 
+    broker.subscribe(REGISTER_EVENT);
+    broker.on(REGISTER_EVENT, () => {
+    });
     app.listen(PORT, () => {
         logger.logI('server started on port:', PORT);
     });
